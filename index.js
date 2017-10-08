5c16b0b5aaad7034a9b1e2d1c04363bc41bc72a3
Lindsay Criswell <lindsaycriswell@gmail.com>
2017-10-08 22:45:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var recipes = {};
 
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
 // function updateObjectWithKeyAndValue(obj, key, value){
 //   return Object.assign({}, obj, { [key]: value })
 // }
